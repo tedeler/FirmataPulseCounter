689db1b246921842f323e9bfa5b173d435b7d610
tedeler <torsten.edeler@haw-hamburg.de>
2020-08-30 14:22:53 +0200

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,9 +1,3 @@
-/* definition to expand macro then apply to pragma message */
-#define VALUE_TO_STRING(x) #x
-#define VALUE(x) VALUE_TO_STRING(x)
-#define VAR_NAME_VALUE(var) #var "="  VALUE(var)
-
-
 #include <utility/boards.h>
 #include <Arduino.h>
 #include <wiring_private.h>
@@ -18,9 +12,6 @@ FirmataExt firmataExt;
 PulseCounterFirmata pulsecounter;
 
 #include <stdio.h>
-
-//#define DEBUG(...) {char buf[128]; sprintf(buf, __VA_ARGS__); SerialUSB.print(buf);}
-#define DEBUG(...) {}
 
 void systemResetCallback()
 {
